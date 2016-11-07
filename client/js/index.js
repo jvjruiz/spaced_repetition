84f7bdea050e6dbec4da3f2f23379b59b89e51e6
MichelleN <dr.mylann@gmail.com>
2016-11-07 17:07:50 +0000

    added react stuff

--- client/js/index.js
+++ client/js/index.js
@@ -1,3 +1,8 @@
 import 'babel-polyfill';
 
 console.log(`Client running in ${process.env.NODE_ENV} mode`);
+
+import React from 'react';
+import Reactdom from 'react-dom';
+
+Reactdom.render(<div>App</div>, document.getElementById("app"));
